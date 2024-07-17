631c406c52928014c5e8720fbeec5642dd3024a9
Denis Maximov <maxidendi@yandex.ru>
2024-07-17 20:51:10 +0500

    Sprint 9: Task done

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    
+    //MARK: - Properties
+    
     static let reuseIdentifier = "ImagesListCell"
     @IBOutlet weak var imageOfCell: UIImageView!
-    @IBOutlet weak var bottomGradient: UILabel!
+    @IBOutlet weak var bottomGradient: UILabel!  {
+        didSet {
+            let layerGradient = CAGradientLayer()
+            layerGradient.colors = [UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 0.00).cgColor,
+                                    UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 0.20).cgColor]
+            layerGradient.frame = bottomGradient.bounds
+            bottomGradient.layer.addSublayer(layerGradient)
+        }
+    }
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var likeButton: UIButton!
 }
