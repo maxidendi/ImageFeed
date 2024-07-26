77024d51d637af5322475473b20a3eb59ca487b6
Denis Maximov <maxidendi@yandex.ru>
2024-07-26 09:51:04 +0500

    add file Constants

---
+++ Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Денис Максимов on 25.07.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "Xm9yREJzofNOp4fxpYuONdi-7ZwiTh-2r-VVZcV_XrM"
+    static let secretKey = "xP9jWM24Ig4-ODmcqEIAmJmz5830HCeKjqLrsZtU5m4"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
